aa376301b1cdac1ca2b803d05e9db931088df9af
Samuel Key <s.key08@gmail.com>
2015-09-16 14:54:10 -0400

    starting devise

--- app/controllers/bottles_controller.rb
+++ app/controllers/bottles_controller.rb
@@ -24,10 +24,25 @@ class BottlesController < ApplicationController
   end
 
   #edit
+  def edit
+    @bottle = Bottle.find(params[:id])
+    @cellar = Cellar.find(params[:id])
+  end
 
   #update
+  def update
+    @bottle = Bottle.find(params[:id])
+    @bottle.update(bottle_params)
+    redirect_to cellar_bottle_path
+  end
 
   #destroy
+  def destroy
+    @bottle = Bottle.find(params[:id])
+    @bottle.destroy
+    @cellar = Cellar.find(params[:cellar_id])
+    redirect_to cellar_path(@cellar)
+  end
 
   private
   def bottle_params
